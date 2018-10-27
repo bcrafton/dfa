76e8a1b8f967b7c8be64d38c7006a794299b1c9c
Brian Crafton <crafton.b@husky.neu.edu>
2018-10-27 10:53:34 -0400

    nice plot

--- sparse_vs_rank/plot_acc_angle1.py
+++ sparse_vs_rank/plot_acc_angle1.py
@@ -7,10 +7,6 @@ import matplotlib.cm as cmap
 
 #######################################
 
-# fig = plt.figure(figsize=(10., 10.))
-
-#######################################
-
 def unit_vector(vector):
     return vector / np.linalg.norm(vector)
 
@@ -83,7 +79,7 @@ for key in data_grouped:
     labels2.append( key )
 
 #######################################
-
+'''
 data = []
 
 itrs = range(1, 10+1)
@@ -144,39 +140,55 @@ labels4 = []
 for key in data_grouped:
     points4.append( data_grouped[key] )
     labels4.append( key )
-
+'''
 #######################################
+
+plt.rcParams['font.sans-serif'] = "Arial"
+plt.rcParams['font.family'] = "sans-serif"
+plt.rcParams['font.size'] = 10
 
 f, [[ax1, ax3], [ax2, ax4]] = plt.subplots(2, 2, sharex=True, sharey=False)
 
 for ii in range(len(points1)):
     p = points1[ii]
     p = np.transpose(p)
-    label = "%s %d" % ('sparse', labels1[ii])
+    label = "%s %d" % ('Sparse', labels1[ii])
     ax1.scatter(p[0], p[1], s=10, label=label)
 
 for ii in range(len(points2)):
     p = points2[ii]
     p = np.transpose(p)
-    label = "%s %d" % ('sparse', labels2[ii])
+    label = "%s %d" % ('Sparse', labels2[ii])
     ax2.scatter(p[0], p[1], s=10, label=label)
 
-for ii in range(len(points3)):
-    p = points3[ii]
+for ii in range(len(points1)):
+    p = points1[ii]
     p = np.transpose(p)
-    label = "%s %d" % ('sparse', labels3[ii])
+    label = "%s %d" % ('Sparse', labels1[ii])
     ax3.scatter(p[0], p[1], s=10, label=label)
 
-for ii in range(len(points4)):
-    p = points4[ii]
+for ii in range(len(points2)):
+    p = points2[ii]
     p = np.transpose(p)
-    label = "%s %d" % ('sparse', labels4[ii])
+    label = "%s %d" % ('Sparse', labels2[ii])
     ax4.scatter(p[0], p[1], s=10, label=label)
 
+# ax2.set_xticks(range(1, 11))
+# ax4.set_xticks(range(1, 11))
+
 f.subplots_adjust(hspace=0)
+f.set_size_inches(8., 6.)
 
-plt.savefig('lol.png')
+lgd = ax4.legend(loc='upper left', bbox_to_anchor=(1.02, 1.5), fontsize=8)
 
+for ax in [ax1, ax2, ax3, ax4]:
+    for tick in ax.yaxis.get_major_ticks():
+        tick.label.set_fontsize(10) 
+    for tick in ax.xaxis.get_major_ticks():
+        tick.label.set_fontsize(10) 
+
+f.savefig('samplefigure', bbox_extra_artists=(lgd,), bbox_inches='tight')
+# plt.show()
 
 
 
