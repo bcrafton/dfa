19f15838ed2f6eb71449dec6713bec5dff37ee5d
Brian Crafton <crafton.b@husky.neu.edu>
2018-10-29 18:27:41 -0400

    plot ops, network, sparse

--- ops/plot_op_alg.py
+++ ops/plot_op_alg.py
@@ -40,41 +40,46 @@ sparses = [1, 5, 10, 25, 100]
 layer_shapes = (4096, 2048, 2048, 100)
 cifar100 = ('CIFAR100', sparses, layer_shapes)
 
-sparses = [1, 5, 10, 25, 100]
-layer_shapes = (4096, 2048, 2048, 100)
-cifar100 = ('CIFAR10', sparses, layer_shapes)
+sparses = [1, 2, 5, 10]
+layer_shapes = (1024 * 3, 1000, 1000, 1000, 10)
+cifar10 = ('CIFAR10', sparses, layer_shapes)
 
-sparses = [1, 5, 10, 25, 100]
-layer_shapes = (4096, 2048, 2048, 100)
-cifar100 = ('MNIST', sparses, layer_shapes)
+sparses = [1, 2, 5, 10]
+layer_shapes = (784, 400, 10)
+mnist = ('MNIST', sparses, layer_shapes)
+
+# benchmarks = [imagenet, cifar100, cifar10, mnist]
+benchmarks = [imagenet, cifar100, mnist]
 
 #######################################
 
 for benchmark in benchmarks:
-    for sparse in sparses:
-        ops = []
-                
+    name, sparses, layer_shapes = benchmark
+    ops = []
+    
+    bp_ops = get_ops(alg='bp', sparse=0, layer_shapes=layer_shapes)
+    dfa_sparsity = layer_shapes[-1]
+    
+    for sparse in sparses: 
         op = get_ops(alg='dfa', sparse=sparse, layer_shapes=layer_shapes)
-        ops.append(op)
+        ops.append(100.0 * op / bp_ops)
 
-    op = get_ops(alg='bp', sparse=0, layer_shapes=layer_shapes)
-    ops.append(op)
-
-    label = benchmark
-    plt.semilogy(sparses, ops, '.', label=label)
+    label = name
+    plt.plot(1.0 * np.array(sparses) / dfa_sparsity, ops, label=label)
+    # plt.semilogy(1.0 * np.array(sparses) / dfa_sparsity, ops, label=label)
 
 #######################################
 
 plt.rcParams['font.sans-serif'] = "Arial"
 plt.rcParams['font.family'] = "sans-serif"
     
-plt.xlabel('Accuracy', fontsize=10)
+plt.xlabel('Sparsity', fontsize=10)
 plt.xticks(fontsize=10)
 
 plt.ylabel('Ops', fontsize=10)
 plt.yticks(fontsize=10)
 
-title = 'Ops vs Accuracy'
+title = 'Ops vs Sparsity'
 plt.title(title, fontsize=10)
 
 plt.legend(fontsize=10)
