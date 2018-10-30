a4686b7af05750e101e44cdd70a39e3dcf0f9bca
Brian Crafton <crafton.b@husky.neu.edu>
2018-10-30 17:14:58 -0400

    only valid data

--- sparse_vs_rank/get_data.py
+++ sparse_vs_rank/get_data.py
@@ -30,26 +30,26 @@ def get_data(benchmark):
 
     for sparse in range(1, 10+1):
         for rank in range(1, 10+1):
-            for itr in itrs:
-            
-                fname = benchmark + "/sparse%drank%ditr%d.npy" % (sparse, rank, itr)
-                results = np.load(fname).item()
-                
-                if benchmark == 'cifar10':
-                    fb = results['fc3_fb']
-                    w2 = results['fc4']
-                else:
-                    fb = results['fc1_fb']
-                    w2 = results['fc2']
-                
-                acc = np.max(results['acc'])
+            if sparse * rank > 10:
+                for itr in itrs:
+                    fname = benchmark + "/sparse%drank%ditr%d.npy" % (sparse, rank, itr)
+                    results = np.load(fname).item()
+                    
+                    if benchmark == 'cifar10':
+                        fb = results['fc3_fb']
+                        w2 = results['fc4']
+                    else:
+                        fb = results['fc1_fb']
+                        w2 = results['fc2']
+                    
+                    acc = np.max(results['acc'])
 
-                # dont forget transpose
-                fb = np.reshape(fb.T, (-1))
-                w2 = np.reshape(w2, (-1))
-                angle = angle_between(fb, w2) * (180. / 3.14)
-                
-                data.append({"sparse":sparse, "rank":rank, "acc":acc, "angle":angle})
+                    # dont forget transpose
+                    fb = np.reshape(fb.T, (-1))
+                    w2 = np.reshape(w2, (-1))
+                    angle = angle_between(fb, w2) * (180. / 3.14)
+                    
+                    data.append({"sparse":sparse, "rank":rank, "acc":acc, "angle":angle})
                 
     return data
     
