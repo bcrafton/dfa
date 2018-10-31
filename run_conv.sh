d848d6d0dc750d75f98357efd1c3657f793c5c72
Brian Crafton <crafton.b@husky.neu.edu>
2018-10-31 17:44:49 -0400

    run scriptS

--- run_conv.sh
+++ run_conv.sh
@@ -1,14 +1,14 @@
 
-python mnist_conv.py --epochs 300 --batch_size 64 --alpha 0.01 --dfa 0 --sparse 0 --init sqrt_fan_in --opt gd --save 1 --name mnist_conv_bp & 
-python mnist_conv.py --epochs 300 --batch_size 64 --alpha 0.01 --dfa 1 --sparse 0 --init zero        --opt gd --save 1 --name mnist_conv_dfa & 
-python mnist_conv.py --epochs 300 --batch_size 64 --alpha 0.01 --dfa 1 --sparse 1 --init zero        --opt gd --save 1 --name mnist_conv_sparse & 
-
-python cifar10_conv.py --epochs 300 --batch_size 64 --alpha 0.005 --dfa 0 --sparse 0 --init sqrt_fan_in --opt adam --save 1 --name cifar10_conv_bp & 
-python cifar10_conv.py --epochs 300 --batch_size 64 --alpha 0.005 --dfa 1 --sparse 0 --init zero        --opt adam --save 1 --name cifar10_conv_dfa & 
-python cifar10_conv.py --epochs 300 --batch_size 64 --alpha 0.005 --dfa 1 --sparse 1 --init zero        --opt adam --save 1 --name cifar10_conv_sparse & 
-
-python cifar100_conv.py --epochs 300 --batch_size 64 --alpha 0.005 --dfa 0 --sparse 0 --init sqrt_fan_in --opt adam --save 1 --name cifar100_conv_bp & 
-python cifar100_conv.py --epochs 300 --batch_size 64 --alpha 0.005 --dfa 1 --sparse 0 --init zero        --opt adam --save 1 --name cifar100_conv_dfa & 
-python cifar100_conv.py --epochs 300 --batch_size 64 --alpha 0.005 --dfa 1 --sparse 1 --init zero        --opt adam --save 1 --name cifar100_conv_sparse & 
+python mnist_conv.py --gpu 0 --epochs 300 --batch_size 64 --alpha 0.01 --dfa 0 --sparse 0 --init sqrt_fan_in --opt gd --save 1 --name mnist_conv_bp & 
+python mnist_conv.py --gpu 1 --epochs 300 --batch_size 64 --alpha 0.01 --dfa 1 --sparse 0 --init zero        --opt gd --save 1 --name mnist_conv_dfa & 
+python mnist_conv.py --gpu 2 --epochs 300 --batch_size 64 --alpha 0.01 --dfa 1 --sparse 1 --init zero        --opt gd --save 1 --name mnist_conv_sparse & 
+wait
+python cifar10_conv.py --gpu 0 --epochs 300 --batch_size 64 --alpha 0.005 --dfa 0 --sparse 0 --init sqrt_fan_in --opt adam --save 1 --name cifar10_conv_bp & 
+python cifar10_conv.py --gpu 1 --epochs 300 --batch_size 64 --alpha 0.005 --dfa 1 --sparse 0 --init zero        --opt adam --save 1 --name cifar10_conv_dfa & 
+python cifar10_conv.py --gpu 2 --epochs 300 --batch_size 64 --alpha 0.005 --dfa 1 --sparse 1 --init zero        --opt adam --save 1 --name cifar10_conv_sparse & 
+wait
+python cifar100_conv.py --gpu 0 --epochs 300 --batch_size 64 --alpha 0.005 --dfa 0 --sparse 0 --init sqrt_fan_in --opt adam --save 1 --name cifar100_conv_bp & 
+python cifar100_conv.py --gpu 1 --epochs 300 --batch_size 64 --alpha 0.005 --dfa 1 --sparse 0 --init zero        --opt adam --save 1 --name cifar100_conv_dfa & 
+python cifar100_conv.py --gpu 2 --epochs 300 --batch_size 64 --alpha 0.005 --dfa 1 --sparse 1 --init zero        --opt adam --save 1 --name cifar100_conv_sparse & 
 
 # we need vgg or alexnet here.
