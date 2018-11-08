be137dd41c2ffab178d06238994efe2bec56d75f
Brian A Crafton <bcrafton3@KL2125-01.ece.gatech.edu>
2018-11-08 10:48:29 -0500

    check the filters

--- run_cifar10.sh
+++ run_cifar10.sh
@@ -1,12 +1,12 @@
 
-python cifar10_conv1.py --dfa 1 --alpha 0.001 &
-python cifar10_conv1.py --dfa 1 --alpha 0.0005 &
-python cifar10_conv1.py --dfa 1 --alpha 0.0001 &
-python cifar10_conv1.py --dfa 1 --alpha 0.00005 &
+python cifar10_conv1.py --epochs 300 --dfa 1 --alpha 0.001 --gpu 0   --name cifar10_conv_dfa_0.001   &
+python cifar10_conv1.py --epochs 300 --dfa 1 --alpha 0.0005 --gpu 1  --name cifar10_conv_dfa_0.0005  &
+python cifar10_conv1.py --epochs 300 --dfa 1 --alpha 0.0001 --gpu 2  --name cifar10_conv_dfa_0.0001  &
+python cifar10_conv1.py --epochs 300 --dfa 1 --alpha 0.00005 --gpu 3 --name cifar10_conv_dfa_0.00005 &
 
-python cifar10_conv1.py --dfa 0 --alpha 0.01 &
-python cifar10_conv1.py --dfa 0 --alpha 0.005 &
-python cifar10_conv1.py --dfa 0 --alpha 0.001 &
-python cifar10_conv1.py --dfa 0 --alpha 0.0005 &
-python cifar10_conv1.py --dfa 0 --alpha 0.0001 &
-python cifar10_conv1.py --dfa 0 --alpha 0.00005 &
+wait 
+
+python cifar10_conv1.py --epochs 300 --dfa 0 --alpha 0.001 --gpu 0   --name cifar10_conv_0.001   &
+python cifar10_conv1.py --epochs 300 --dfa 0 --alpha 0.0005 --gpu 1  --name cifar10_conv_0.0005  &
+python cifar10_conv1.py --epochs 300 --dfa 0 --alpha 0.0001 --gpu 2  --name cifar10_conv_0.0001  &
+python cifar10_conv1.py --epochs 300 --dfa 0 --alpha 0.00005 --gpu 3 --name cifar10_conv_0.00005 &
