e9ab90d01d481431cc3ade98777d3311fdede7b0
Brian Crafton <crafton.b@husky.neu.edu>
2018-11-09 14:13:32 -0500

    plotting the conv weights

--- weights/plot.py
+++ weights/plot.py
@@ -4,9 +4,11 @@ import matplotlib.cm as cmap
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-weights = np.load('imagenet/conv1_dfa.npy')
+weights = np.load('cifar10_conv.npy').item()
+weights = weights['conv1']
 weights = np.transpose(weights)
-weights = np.reshape(weights, (16, 18, 11, 11))
+# 96 * 3
+weights = np.reshape(weights, (16, 18, 5, 5))
 
 for ii in range(16):
     for jj in range(18):
@@ -20,4 +22,26 @@ for ii in range(16):
     else:
         img = np.concatenate((img, row), axis=0)
   
-plt.imsave("alexnet_dfa.png", img, cmap="gray")
+plt.imsave("cifar10.png", img, cmap="gray")
+
+###########################################################
+
+weights = np.load('cifar10_conv_dfa.npy').item()
+weights = weights['conv1']
+weights = np.transpose(weights)
+# 96 * 3
+weights = np.reshape(weights, (16, 18, 5, 5))
+
+for ii in range(16):
+    for jj in range(18):
+        if jj == 0:
+            row = weights[ii][jj]
+        else:
+            row = np.concatenate((row, weights[ii][jj]), axis=1)
+            
+    if ii == 0:
+        img = row
+    else:
+        img = np.concatenate((img, row), axis=0)
+  
+plt.imsave("cifar10_dfa.png", img, cmap="gray")
