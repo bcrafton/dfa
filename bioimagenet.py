7f660fe5dbb64283a2ca1f9d1aad9fc7fa947a1c
Brian Crafton <crafton.b@husky.neu.edu>
2018-11-26 15:00:41 -0500

    why not

---
+++ bioimagenet.py
@@ -0,0 +1,425 @@
+
+import argparse
+import os
+import sys
+
+##############################################
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--epochs', type=int, default=100)
+parser.add_argument('--batch_size', type=int, default=128)
+parser.add_argument('--alpha', type=float, default=1e-2)
+parser.add_argument('--decay', type=float, default=0.99)
+parser.add_argument('--gpu', type=int, default=0)
+parser.add_argument('--dfa', type=int, default=0)
+parser.add_argument('--sparse', type=int, default=0)
+parser.add_argument('--rank', type=int, default=0)
+parser.add_argument('--init', type=str, default="sqrt_fan_in")
+parser.add_argument('--opt', type=str, default="gd")
+parser.add_argument('--save', type=int, default=0)
+parser.add_argument('--name', type=str, default="imagenet_alexnet")
+args = parser.parse_args()
+
+if args.gpu >= 0:
+    os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"]=str(args.gpu)
+
+##############################################
+
+import keras
+from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD
+
+import tensorflow as tf
+import os
+import math
+import numpy
+import numpy as np
+np.set_printoptions(threshold=1000)
+import time
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+from PIL import Image
+import scipy.misc
+
+from Model import Model
+
+from Layer import Layer 
+from ConvToFullyConnected import ConvToFullyConnected
+from FullyConnected import FullyConnected
+from Convolution import Convolution
+from BioConvolution import BioConvolution
+from MaxPool import MaxPool
+from Dropout import Dropout
+from FeedbackFC import FeedbackFC
+from FeedbackConv import FeedbackConv
+
+from Activation import Activation
+from Activation import Sigmoid
+from Activation import Relu
+from Activation import Tanh
+from Activation import Softmax
+from Activation import LeakyRelu
+from Activation import Linear
+
+##############################################
+
+batch_size = args.batch_size
+num_classes = 1000
+epochs = args.epochs
+data_augmentation = False
+
+EPOCHS = args.epochs
+BATCH_SIZE = args.batch_size
+
+IMAGENET_MEAN = [123.68, 116.78, 103.94]
+
+##############################################
+
+# https://gist.githubusercontent.com/omoindrot/dedc857cdc0e680dfb1be99762990c9c/raw/e560edd240f8b97e1f0483843dc4d64729ce025c/tensorflow_finetune.py
+
+# Preprocessing (for both training and validation):
+# (1) Decode the image from jpg format
+# (2) Resize the image so its smaller side is 256 pixels long
+def parse_function(filename, label):
+    image_string = tf.read_file(filename)
+    image_decoded = tf.image.decode_jpeg(image_string, channels=3)          # (1)
+    image = tf.cast(image_decoded, tf.float32)
+
+    smallest_side = 256.0
+    height, width = tf.shape(image)[0], tf.shape(image)[1]
+    height = tf.to_float(height)
+    width = tf.to_float(width)
+
+    scale = tf.cond(tf.greater(height, width),
+                    lambda: smallest_side / width,
+                    lambda: smallest_side / height)
+    new_height = tf.to_int32(height * scale)
+    new_width = tf.to_int32(width * scale)
+
+    resized_image = tf.image.resize_images(image, [new_height, new_width])  # (2)
+    return resized_image, label
+
+# Preprocessing (for training)
+# (3) Take a random 225x225 crop to the scaled image
+# (4) Horizontally flip the image with probability 1/2
+# (5) Substract the per color mean `IMAGENET_MEAN`
+# Note: we don't normalize the data here, as VGG was trained without normalization
+def train_preprocess(image, label):
+    crop_image = tf.random_crop(image, [225, 225, 3])                       # (3)
+    flip_image = tf.image.random_flip_left_right(crop_image)                # (4)
+
+    means = tf.reshape(tf.constant(IMAGENET_MEAN), [1, 1, 3])
+    centered_image = flip_image - means                                     # (5)
+
+    return centered_image, label
+    
+
+# Preprocessing (for validation)
+# (3) Take a central 225x225 crop to the scaled image
+# (4) Substract the per color mean `IMAGENET_MEAN`
+# Note: we don't normalize the data here, as VGG was trained without normalization
+def val_preprocess(image, label):
+    crop_image = tf.image.resize_image_with_crop_or_pad(image, 225, 225)    # (3)
+
+    means = tf.reshape(tf.constant(IMAGENET_MEAN), [1, 1, 3])
+    centered_image = crop_image - means                                     # (4)
+
+    return centered_image, label
+
+##############################################
+
+def get_validation_dataset():
+    label_counter = 0
+    validation_images = []
+    validation_labels = []
+
+    print ("building validation dataset")
+
+    for subdir, dirs, files in os.walk('/home/bcrafton3/ILSVRC2012/val/'):
+        for file in files:
+            validation_images.append(os.path.join('/home/bcrafton3/ILSVRC2012/val/', file))
+    validation_images = sorted(validation_images)
+
+    validation_labels_file = open('/home/bcrafton3/dfa/imagenet_labels/validation_labels.txt')
+    lines = validation_labels_file.readlines()
+    for ii in range(len(lines)):
+        validation_labels.append(int(lines[ii]))
+
+    print (len(validation_images), len(validation_labels))
+    remainder = len(validation_labels) % batch_size
+    validation_images = validation_images[:(-remainder)]
+    validation_labels = validation_labels[:(-remainder)]
+
+    print("validation data is ready...")
+
+    return validation_images, validation_labels
+    
+def get_train_dataset():
+
+    label_counter = 0
+    training_images = []
+    training_labels = []
+
+    print ("making labels dict")
+
+    f = open('/home/bcrafton3/dfa/imagenet_labels/train_labels.txt', 'r')
+    lines = f.readlines()
+
+    labels = {}
+    for line in lines:
+        line = line.split(' ')
+        labels[line[0]] = label_counter
+        label_counter += 1
+
+    f.close()
+
+    print ("building dataset")
+
+    for subdir, dirs, files in os.walk('/home/bcrafton3/ILSVRC2012/train/'):
+        for folder in dirs:
+            for folder_subdir, folder_dirs, folder_files in os.walk(os.path.join(subdir, folder)):
+                for file in folder_files:
+                    training_images.append(os.path.join(folder_subdir, file))
+                    training_labels.append(labels[folder])
+
+    remainder = len(training_labels) % batch_size
+    training_images = training_images[:(-remainder)]
+    training_labels = training_labels[:(-remainder)]
+
+    print("Data is ready...")
+
+    return training_images, training_labels
+
+###############################################################
+
+filename = tf.placeholder(tf.string, shape=[None])
+label = tf.placeholder(tf.int64, shape=[None])
+
+###############################################################
+
+val_imgs, val_labs = get_validation_dataset()
+
+val_dataset = tf.data.Dataset.from_tensor_slices((filename, label))
+val_dataset = val_dataset.shuffle(len(val_imgs))
+val_dataset = val_dataset.map(parse_function, num_parallel_calls=4)
+val_dataset = val_dataset.map(val_preprocess, num_parallel_calls=4)
+val_dataset = val_dataset.batch(batch_size)
+val_dataset = val_dataset.repeat()
+val_dataset = val_dataset.prefetch(8)
+
+###############################################################
+
+train_imgs, train_labs = get_train_dataset()
+
+train_dataset = tf.data.Dataset.from_tensor_slices((filename, label))
+train_dataset = train_dataset.shuffle(len(train_imgs))
+train_dataset = train_dataset.map(parse_function, num_parallel_calls=4)
+train_dataset = train_dataset.map(train_preprocess, num_parallel_calls=4)
+train_dataset = train_dataset.batch(batch_size)
+train_dataset = train_dataset.repeat()
+train_dataset = train_dataset.prefetch(8)
+
+###############################################################
+
+handle = tf.placeholder(tf.string, shape=[])
+iterator = tf.data.Iterator.from_string_handle(handle, train_dataset.output_types, train_dataset.output_shapes)
+features, labels = iterator.get_next()
+features = tf.reshape(features, (-1, 225, 225, 3))
+labels = tf.one_hot(labels, depth=num_classes)
+
+train_iterator = train_dataset.make_initializable_iterator()
+val_iterator = val_dataset.make_initializable_iterator()
+
+###############################################################
+
+train_conv=True
+train_fc=True
+weights_conv=None
+weights_fc=None
+
+if args.dfa:
+    bias = 0.0
+else:
+    bias = 0.0
+
+###############################################################
+
+dropout_rate = tf.placeholder(tf.float32, shape=())
+learning_rate = tf.placeholder(tf.float32, shape=())
+
+l0 = BioConvolution(input_sizes=[batch_size, 225, 225, 3], filter_sizes=[5, 5, 3, 32], num_classes=num_classes, init_filters=args.init, strides=[1, 5, 5, 1], padding="VALID", alpha=learning_rate, activation=Relu(), bias=0., last_layer=False, name="conv1", load=weights_conv, train=train_conv)
+
+l1 = BioConvolution(input_sizes=[batch_size, 45, 45, 32], filter_sizes=[3, 3, 32, 32], num_classes=num_classes, init_filters=args.init, strides=[1, 3, 3, 1], padding="SAME", alpha=learning_rate, activation=Relu(), bias=1., last_layer=False, name="conv2", load=weights_conv, train=train_conv)
+
+l2 = BioConvolution(input_sizes=[batch_size, 15, 15, 32], filter_sizes=[3, 3, 32, 32], num_classes=num_classes, init_filters=args.init, strides=[1, 1, 1, 1], padding="SAME", alpha=learning_rate, activation=Relu(), bias=bias, last_layer=False, name="conv3", load=weights_conv, train=train_conv)
+
+l5 = BioConvolution(input_sizes=[batch_size, 15, 15, 32], filter_sizes=[3, 3, 32, 32], num_classes=num_classes, init_filters=args.init, strides=[1, 1, 1, 1], padding="SAME", alpha=learning_rate, activation=Relu(), bias=1., last_layer=False, name="conv4", load=weights_conv, train=train_conv)
+
+l5 = BioConvolution(input_sizes=[batch_size, 15, 15, 32], filter_sizes=[3, 3, 32, 128], num_classes=num_classes, init_filters=args.init, strides=[1, 3, 3, 1], padding="SAME", alpha=learning_rate, activation=Relu(), bias=1., last_layer=False, name="conv4", load=weights_conv, train=train_conv)
+
+l8 = ConvToFullyConnected(shape=[5, 5, 128])
+
+l9 = FullyConnected(size=[5*5*128, 2048], num_classes=num_classes, init_weights=args.init, alpha=learning_rate, activation=Relu(), bias=1., last_layer=False, name="fc1", load=weights_fc, train=train_fc)
+l10 = Dropout(rate=dropout_rate)
+
+l11 = FullyConnected(size=[2048, 2048], num_classes=num_classes, init_weights=args.init, alpha=learning_rate, activation=Relu(), bias=1., last_layer=False, name="fc2", load=weights_fc, train=train_fc)
+l12 = Dropout(rate=dropout_rate)
+
+l13 = FullyConnected(size=[2048, num_classes], num_classes=num_classes, init_weights=args.init, alpha=learning_rate, activation=Linear(), bias=1., last_layer=True, name="fc3", load=weights_fc, train=train_fc)
+
+###############################################################
+
+# model = Model(layers=[l0, l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12, l13, l14, l15, l16, l17, l18])
+model = Model(layers=[l0, l1, l3, l4, l6, l8, l10, l11, l13, l14, l15, l17, l18, l20])
+
+predict = tf.nn.softmax(model.predict(X=features))
+
+if args.opt == "adam" or args.opt == "rms" or args.opt == "decay" or args.opt = "momentum":
+    if args.dfa:
+        grads_and_vars = model.dfa_gvs(X=features, Y=labels)
+    else:
+        grads_and_vars = model.gvs(X=features, Y=labels)
+        
+    if args.opt == "adam":
+        train = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=0.9, beta2=0.999, epsilon=1.0).apply_gradients(grads_and_vars=grads_and_vars)
+    elif args.opt == "rms":
+        train = tf.train.RMSPropOptimizer(learning_rate=learning_rate, decay=0.99, epsilon=1.0).apply_gradients(grads_and_vars=grads_and_vars)
+    elif args.opt == "decay":
+        train = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).apply_gradients(grads_and_vars=grads_and_vars)
+    elif args.opt == "momentum":
+        train = tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=0.9).apply_gradients(grads_and_vars=grads_and_vars)
+    else:
+        assert(False)
+
+else:
+    if args.dfa:
+        train = model.dfa(X=features, Y=labels)
+    else:
+        train = model.train(X=features, Y=labels)
+
+correct = tf.equal(tf.argmax(predict,1), tf.argmax(labels,1))
+total_correct = tf.reduce_sum(tf.cast(correct, tf.float32))
+accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+weights = model.get_weights()
+
+print (model.num_params())
+
+###############################################################
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth=True
+sess = tf.InteractiveSession(config=config)
+tf.global_variables_initializer().run()
+
+train_handle = sess.run(train_iterator.string_handle())
+val_handle = sess.run(val_iterator.string_handle())
+
+###############################################################
+
+results_filename = args.name + '.results'
+f = open(results_filename, "w")
+f.write(results_filename + "\n")
+f.write("total params: " + str(model.num_params()) + "\n")
+f.close()
+
+###############################################################
+
+train_accs = []
+val_accs = []
+
+alpha = args.alpha
+phase = 0
+
+for ii in range(0, epochs):
+
+    '''
+    if args.opt == 'decay' or args.opt == 'gd' or args.opt == 'momentum':
+        decay = np.power(args.decay, ii)
+        lr = args.alpha * decay
+    else:
+        lr = args.alpha
+    '''
+
+    print (ii)
+
+    sess.run(train_iterator.initializer, feed_dict={filename: train_imgs, label: train_labs})
+    train_correct = 0.0
+    train_total = 0.0
+    # for j in range(0, 32 * 10, batch_size):
+    for j in range(0, len(train_imgs), batch_size):
+        print (j)
+        
+        _total_correct, _ = sess.run([total_correct, train], feed_dict={handle: train_handle, dropout_rate: 0.5, learning_rate: lr})
+        train_correct += _total_correct
+        train_total += batch_size
+        train_acc = train_correct / train_total
+        
+        print ("train accuracy: " + str(train_acc))     
+        f = open(results_filename, "a")
+        f.write(str(train_acc) + "\n")
+        f.close()
+
+    train_accs.append(train_acc)
+
+    sess.run(val_iterator.initializer, feed_dict={filename: val_imgs, label: val_labs})
+    val_correct = 0.0
+    val_total = 0.0
+    lr = 0.0
+    # for j in range(0, 32 * 10, batch_size):
+    for j in range(0, len(val_imgs), batch_size):
+        print (j)
+
+        [_total_correct] = sess.run([total_correct], feed_dict={handle: val_handle, dropout_rate: 0.0, learning_rate: lr})
+        val_correct += _total_correct
+        val_total += batch_size
+        val_acc = val_correct / val_total
+        
+        print ("val accuracy: " + str(val_acc))
+        f = open(results_filename, "a")
+        f.write(str(val_acc) + "\n")
+        f.close()
+
+    val_accs.append(val_acc)
+
+    if phase == 0:
+        phase = 1
+        print ('phase 1')
+    elif phase == 1:
+        dacc = test_accs[-1] - test_accs[-2]
+        if dacc <= 0.01:
+            alpha = 0.001
+            phase = 2
+            print ('phase 2')
+    elif phase == 2:
+        dacc = test_accs[-1] - test_accs[-2]
+        if dacc <= 0.001:
+            alpha = 0.0001
+            phase = 3
+            print ('phase 3')
+
+    if args.save:
+        [w] = sess.run([weights], feed_dict={handle: val_handle, dropout_rate: 0.0, learning_rate: lr})
+        w['train_acc'] = train_accs
+        w['val_acc'] = val_accs
+        np.save(args.name, w)
+
+    print('epoch {}/{}'.format(ii, epochs))
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
