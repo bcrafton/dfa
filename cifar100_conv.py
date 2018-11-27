f7814e16f2cf67e58759a589abaf51df46a9450a
Brian A Crafton <bcrafton3@KL2125-01.ece.gatech.edu>
2018-11-27 00:38:33 -0500

    Save file right name

--- cifar100_conv.py
+++ cifar100_conv.py
@@ -226,7 +226,7 @@ for ii in range(EPOCHS):
     print ("train acc: %f test acc: %f" % (train_acc, test_acc))
     
     f = open(filename, "a")
-    f.write(str(test_acc) + "\n")
+    f.write("train acc: %f test acc: %f\n" % (train_acc, test_acc))
     f.close()
 
 ##############################################
