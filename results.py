e3f9f6794068d5b1a7656a4cb55ecbc497435c8c
Brian Crafton <crafton.b@husky.neu.edu>
2018-12-05 10:10:54 -0500

    fixing results for imagenet lel

--- results.py
+++ results.py
@@ -26,84 +26,29 @@ def get_perms(param):
 
 ################################################
 
-# mnist_fc_bp = {'benchmark':'mnist_fc.py', 'epochs':300, 'batch_size':32, 'alpha':[0.05, 0.03], 'dfa':0, 'sparse':0, 'rank':0, 'init':'sqrt_fan_in', 'opt':'gd', 'load':None}
-# mnist_fc_dfa = {'benchmark':'mnist_fc.py', 'epochs':300, 'batch_size':32, 'alpha':[0.1, 0.075, 0.05, 0.03], 'dfa':1, 'sparse':[0, 1], 'rank':0, 'init':'zero', 'opt':'gd', 'load':None}
+mnist_fc_lel = {'benchmark':'mnist_fc.py', 'epochs':300, 'batch_size':32, 'alpha':[1e-4, 3e-5, 1e-5], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'alg':['lel'], 'sparse':[0, 1], 'rank':0, 'init':'zero', 'opt':'gd', 'load':[None]}
+mnist_fc_sparse = {'benchmark':'mnist_fc.py', 'epochs':300, 'batch_size':32, 'alpha':[1e-4, 3e-5, 1e-5], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'alg':['lel'], 'sparse':[0, 1], 'rank':0, 'init':'zero', 'opt':'gd', 'load':[None]}
 
-cifar10_fc_bp = {'benchmark':'cifar10_fc.py', 'epochs':500, 'batch_size':64, 'alpha':[1e-4, 3e-5, 1e-5], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'dfa':0, 'sparse':0, 'rank':0, 'init':'sqrt_fan_in', 'opt':['adam'], 'load':None}
-cifar10_fc_dfa = {'benchmark':'cifar10_fc.py', 'epochs':500, 'batch_size':64, 'alpha':[1e-4, 3e-5, 1e-5], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'dfa':1, 'sparse':[0], 'rank':0, 'init':'zero', 'opt':['adam'], 'load':None}
-cifar10_fc_sparse = {'benchmark':'cifar10_fc.py', 'epochs':500, 'batch_size':64, 'alpha':[1e-4, 3e-5, 1e-5], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'dfa':1, 'sparse':[1], 'rank':0, 'init':'zero', 'opt':['adam'], 'load':None}
+cifar10_fc_lel = {'benchmark':'cifar10_fc.py', 'epochs':300, 'batch_size':64, 'alpha':[1e-4, 3e-5, 1e-5], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'alg':['lel'], 'sparse':[0], 'rank':0, 'init':'zero', 'opt':['adam'], 'load':[None]}
+cifar10_fc_lel_sparse = {'benchmark':'cifar10_fc.py', 'epochs':300, 'batch_size':64, 'alpha':[1e-4, 3e-5, 1e-5], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'alg':['lel'], 'sparse':[1], 'rank':0, 'init':'zero', 'opt':['adam'], 'load':[None]}
 
-cifar100_fc_bp = {'benchmark':'cifar100_fc.py', 'epochs':300, 'batch_size':64, 'alpha':[1e-4, 3e-5, 1e-5], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'dfa':0, 'sparse':0, 'rank':0, 'init':'sqrt_fan_in', 'opt':['adam'], 'load':None}
-cifar100_fc_dfa = {'benchmark':'cifar100_fc.py', 'epochs':500, 'batch_size':64, 'alpha':[1e-4, 3e-5, 1e-5], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'dfa':1, 'sparse':[0], 'rank':0, 'init':'zero', 'opt':['adam'], 'load':None}
-cifar100_fc_sparse = {'benchmark':'cifar100_fc.py', 'epochs':2000, 'batch_size':64, 'alpha':[1e-4, 3e-5, 1e-5], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'dfa':1, 'sparse':[1], 'rank':0, 'init':'zero', 'opt':['adam'], 'load':None}
-# cifar100_fc_sparse = {'benchmark':'cifar100_fc.py', 'epochs':2000, 'batch_size':64, 'alpha':[1e-4, 3e-5, 1e-5], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.0, 0.1], 'dfa':1, 'sparse':[1], 'rank':0, 'init':'zero', 'opt':['adam'], 'load':None}
+cifar100_fc_lel = {'benchmark':'cifar100_fc.py', 'epochs':300, 'batch_size':64, 'alpha':[1e-4, 3e-5, 1e-5], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'alg':['lel'], 'sparse':[0], 'rank':0, 'init':'zero', 'opt':['adam'], 'load':[None]}
+cifar100_fc_lel_sparse = {'benchmark':'cifar100_fc.py', 'epochs':300, 'batch_size':64, 'alpha':[1e-4, 3e-5, 1e-5], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'alg':['lel'], 'sparse':[1], 'rank':0, 'init':'zero', 'opt':['adam'], 'load':[None]}
 
 ################################################
 
-mnist_conv_bp = {'benchmark':'mnist_conv.py', 'epochs':300, 'batch_size':64, 'alpha':[0.01, 0.005], 'dfa':0, 'sparse':0, 'rank':0, 'init':'sqrt_fan_in', 'opt':'gd', 'load':[None, './transfer/mnist_conv_weights.npy']}
-mnist_conv_dfa = {'benchmark':'mnist_conv.py', 'epochs':300, 'batch_size':64, 'alpha':[0.01, 0.005], 'dfa':1, 'sparse':[0, 1], 'rank':0, 'init':'zero', 'opt':'gd', 'load':[None, './transfer/mnist_conv_weights.npy']}
+mnist_conv_lel = {'benchmark':'mnist_conv.py', 'epochs':300, 'batch_size':64, 'alpha':[0.01, 0.005], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'alg':['lel'], 'sparse':[0, 1], 'rank':0, 'init':'zero', 'opt':'gd', 'load':[None]}
+mnist_conv_lel_sparse = {'benchmark':'mnist_conv.py', 'epochs':300, 'batch_size':64, 'alpha':[0.01, 0.005], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'alg':['lel'], 'sparse':[0, 1], 'rank':0, 'init':'zero', 'opt':'gd', 'load':[None]}
 
-cifar10_conv_bp = {'benchmark':'cifar10_conv.py', 'epochs':500, 'batch_size':64, 'alpha':[1e-4, 3e-5, 1e-5], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'dfa':0, 'sparse':0, 'rank':0, 'init':'sqrt_fan_in', 'opt':['adam'], 'load':None}
-cifar10_conv_dfa = {'benchmark':'cifar10_conv.py', 'epochs':500, 'batch_size':64, 'alpha':[1e-4, 3e-5, 1e-5], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'dfa':1, 'sparse':[0], 'rank':0, 'init':'zero', 'opt':['adam'], 'load':None}
-cifar10_conv_sparse = {'benchmark':'cifar10_conv.py', 'epochs':500, 'batch_size':64, 'alpha':[1e-4, 3e-5, 1e-5], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'dfa':1, 'sparse':[1], 'rank':0, 'init':'zero', 'opt':['adam'], 'load':None}
+cifar10_conv_lel = {'benchmark':'cifar10_conv.py', 'epochs':300, 'batch_size':64, 'alpha':[1e-4, 3e-5, 1e-5], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'alg':['lel'], 'sparse':[0], 'rank':0, 'init':'zero', 'opt':['adam'], 'load':[None]}
+cifar10_conv_lel_sparse = {'benchmark':'cifar10_conv.py', 'epochs':300, 'batch_size':64, 'alpha':[1e-4, 3e-5, 1e-5], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'alg':['lel'], 'sparse':[1], 'rank':0, 'init':'zero', 'opt':['adam'], 'load':[None]}
 
-cifar100_conv_bp = {'benchmark':'cifar100_conv.py', 'epochs':300, 'batch_size':64, 'alpha':[1e-4, 3e-5, 1e-5], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'dfa':0, 'sparse':0, 'rank':0, 'init':'sqrt_fan_in', 'opt':['adam'], 'load':None}
-cifar100_conv_dfa = {'benchmark':'cifar100_conv.py', 'epochs':500, 'batch_size':64, 'alpha':[1e-4, 3e-5, 1e-5], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'dfa':1, 'sparse':[0], 'rank':0, 'init':'zero', 'opt':['adam'], 'load':None}
-cifar100_conv_sparse = {'benchmark':'cifar100_conv.py', 'epochs':500, 'batch_size':64, 'alpha':[1e-4, 3e-5, 1e-5], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'dfa':1, 'sparse':[1], 'rank':0, 'init':'zero', 'opt':['adam'], 'load':None}
-
-################################################
-'''
-imagenet_alexnet_bp = {'benchmark':'imagenet.py', 'epochs':100, 'batch_size':128, 'alpha':[0.01], 'dfa':0, 'sparse':0, 'rank':0, 'init':['NA', 'alexnet'], 'opt':'gd', 'load':None}
-imagenet_vgg_bp = {'benchmark':'imagenet_vgg.py', 'epochs':100, 'batch_size':32, 'alpha':[0.01], 'dfa':0, 'sparse':0, 'rank':0, 'init':['sqrt_fan_in'], 'opt':'gd', 'load':None}
-'''
-################################################
-
-cifar100_fc_bp = {'benchmark':'cifar100_fc_2048.py', 'epochs':300, 'batch_size':64, 'alpha':[1e-4, 3e-5, 1e-5], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'dfa':0, 'sparse':0, 'rank':0, 'init':'sqrt_fan_in', 'opt':['adam'], 'load':None}
-cifar100_fc_dfa = {'benchmark':'cifar100_fc_2048.py', 'epochs':500, 'batch_size':64, 'alpha':[1e-4, 3e-5, 1e-5], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'dfa':1, 'sparse':[0], 'rank':0, 'init':'zero', 'opt':['adam'], 'load':None}
-cifar100_fc_sparse = {'benchmark':'cifar100_fc_2048.py', 'epochs':1000, 'batch_size':64, 'alpha':[1e-4, 3e-5, 1e-5], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'dfa':1, 'sparse':[1], 'rank':0, 'init':'zero', 'opt':['adam'], 'load':None}
+cifar100_conv_lel = {'benchmark':'cifar100_conv.py', 'epochs':300, 'batch_size':64, 'alpha':[1e-4, 3e-5, 1e-5], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'alg':['lel'], 'sparse':[0], 'rank':0, 'init':'zero', 'opt':['adam'], 'load':[None]}
+cifar100_conv_lel_sparse = {'benchmark':'cifar100_conv.py', 'epochs':300, 'batch_size':64, 'alpha':[1e-4, 3e-5, 1e-5], 'eps':[1e-4, 1e-5, 1e-6], 'act':['tanh', 'relu'], 'dropout':[0.25, 0.5], 'alg':['lel'], 'sparse':[1], 'rank':0, 'init':'zero', 'opt':['adam'], 'load':[None]}
 
 ################################################
 
-'''
-params = [mnist_fc_bp,      \
-          mnist_fc_dfa,     \
-          cifar10_fc_bp,    \
-          cifar10_fc_dfa,   \
-          cifar100_fc_bp,   \
-          cifar100_fc_dfa,  \
-          mnist_conv_bp,    \
-          mnist_conv_dfa,   \
-          cifar10_conv_bp,  \ 
-          cifar10_conv_dfa, \
-          cifar100_conv_bp, \
-          cifar100_conv_dfa]
-'''
-
-# params = [imagenet_alexnet_bp, imagenet_vgg_bp]
-# params = [imagenet_alexnet_bp]
-# params = [imagenet_vgg_bp]
-
-# params = [cifar10_fc_dfa]
-# params = [cifar10_fc_bp]
-# params = [cifar10_fc_bp, cifar10_fc_dfa]
-
-# params = [cifar100_fc_bp, cifar100_fc_dfa]
-# params = [cifar100_fc_dfa_adam, cifar100_fc_dfa_gd]
-# params = [cifar100_fc_bp]
-
-# params = [cifar10_conv_bp, cifar10_conv_dfa, cifar100_conv_bp, cifar100_conv_dfa]
-# params = [cifar100_conv_bp, cifar100_conv_dfa]
-# params = [cifar10_conv_sparse, cifar100_conv_sparse]
-
-# params = [cifar10_fc_bp, cifar10_fc_dfa, cifar10_fc_sparse, cifar100_fc_bp, cifar100_fc_dfa, cifar100_fc_sparse]
-# params = [cifar10_conv_bp, cifar10_conv_dfa, cifar10_conv_sparse, cifar100_conv_bp, cifar100_conv_dfa, cifar100_conv_sparse]
-
-# params = [cifar100_fc_sparse]
-
-# params = [cifar10_fc_bp, cifar10_fc_dfa, cifar10_fc_sparse, cifar100_fc_bp, cifar100_fc_dfa, cifar100_fc_sparse, cifar10_conv_bp, cifar10_conv_dfa, cifar10_conv_sparse, cifar100_conv_bp, cifar100_conv_dfa, cifar100_conv_sparse]
-
-#params = [cifar100_fc_bp, cifar100_fc_dfa, cifar100_fc_sparse]
-params = [cifar100_fc_sparse]
+params = [mnist_fc_lel, cifar10_fc_lel, cifar100_fc_lel, mnist_conv_lel, cifar10_conv_lel, cifar100_conv_lel]
 
 ################################################
 
