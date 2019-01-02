d0235334c90083d6a56e4db94f0eda2cd4b8edaf
Brian Crafton <crafton.b@husky.neu.edu>
2019-01-02 16:43:41 -0500

    actually plot

--- plot_gradient.py
+++ plot_gradient.py
@@ -13,38 +13,37 @@ runs = get_runs()
 
 ##############################################
 
-
+f, axes = plt.subplots(3, 3, sharex=False, sharey=False)
 
 num_runs = len(runs)
 for ii in range(num_runs):
     param = runs[ii]
 
-    # figure out the name of the param
     name = '%s_%f_%f_%s_%f_%f_%d_%d_%s_%s' % (param['benchmark'], param['alpha'], param['eps'], param['act'], param['bias'], param['dropout'], param['dfa'], param['sparse'], param['init'], param['opt'])
     if param['load']:
         name += '_transfer'
     name = name + '.npy'
 
-    # load the results
     res = np.load(name).item()
 
+    fc1 = np.std(res['fc1'])
     dfc1 = res['dfc1']
     dfc1_bias = res['dfc1_bias']
+    a1 = res['A1']
     
+    fc2 = np.std(res['fc2'])
     dfc2 = res['dfc2']
     dfc2_bias = res['dfc2_bias']
+    a2 = res['A2']
     
+    fc3 = np.std(res['fc3'])
     dfc3 = res['dfc3']
     dfc3_bias = res['dfc3_bias']
+    a3 = res['A3']
     
-    results = {}
+    label = '%f_%d' % (param['alpha'], param['dfa'])
+    axes[0].plot(dfc1, label=label)
+    axes[1].plot(dfc2, label=label)
+    axes[2].plot(dfc3, label=label)
     
-    results['dfc1'] =      dfc1
-    results['dfc1_bias'] = dfc1_bias
-    results['dfc2'] =      dfc2
-    results['dfc2_bias'] = dfc2_bias
-    results['dfc3'] =      dfc3
-    results['dfc3_bias'] = dfc3_bias
-
-    name = 'gradients_' + name
-    np.save(name, results)
+plt.savefig('gradients.png')
